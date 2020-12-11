bc5985c0d369e99f16409781ffa38df394bda1b0
I-Am-Lain <vetter.william@gmail.com>
2020-12-11 04:25:55 -0600

    SMS functionality

--- src/components/BasicComponent.js
+++ src/components/BasicComponent.js
@@ -1,10 +1,21 @@
 import React from 'react'
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Button } from 'react-native';
+import { StyleSheet, Text, View, Button, TouchableOpacity } from 'react-native';
 import { setAdvanced, addTodo } from '../actions/index.js'
 
 import { connect } from 'react-redux'
 
+import * as SMS from 'expo-sms';
+
+const handlePress = async () => {
+    const isAvailable = await SMS.isAvailableAsync();
+    if (isAvailable) {
+        const { result } = await SMS.sendSMSAsync(
+            '7346461641', 'My sample HelloWorld message')
+    } else {
+        alert('message didnt work dawg')
+    }
+}
 
 
 const BasicComponent = (props) => {
@@ -20,6 +31,12 @@ const BasicComponent = (props) => {
             <View style={styles.buttonContainer}>
                 <Button title="Hey I'm a button!" onPress={() => props.addTodo('yep')}/>
             </View>
+
+                  <TouchableOpacity
+                    onPress={handlePress}
+                    style={{ backgroundColor: 'blue' }}>
+                    <Text style={{ fontSize: 20, color: '#fff' }}>Pick a photo</Text>
+                </TouchableOpacity>
 
             <StatusBar style="auto" />
         </View>
